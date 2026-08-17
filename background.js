701e1eda27c1ba98a1840523dfa8fed7e18d8230
Valentin Grtz <39189263+ValentinGratz@users.noreply.github.com>
2026-08-17 20:20:31 +0200

    Refactor message listener and cleanup code

--- background.js
+++ background.js
@@ -1,19 +1,9 @@
-// background.js - FIX CORS - fetch via background (a les permissions)
-chrome.runtime.onInstalled.addListener(()=>{ chrome.storage.local.clear(); console.log('[V2L] storage cleared'); });
-
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse)=>{
-  if(msg.type === 'FETCH_IMAGE'){
-    fetch(msg.url)
-      .then(r=>r.blob())
-      .then(async blob=>{
-        const buffer = await blob.arrayBuffer();
-        sendResponse({ok:true, buffer: Array.from(new Uint8Array(buffer)), type: blob.type});
-      })
-      .catch(err=> sendResponse({ok:false, error: err.toString()}));
-    return true; // async
-  }
-  if(msg.type === 'CLEAR_STORAGE'){
-    chrome.storage.local.clear(()=> sendResponse({ok:true}));
+chrome.runtime.onMessage.addListener((msg,s,sendResponse)=>{
+  if(msg.type==='FETCH_IMAGE'){
+    fetch(msg.url).then(r=>r.blob()).then(async blob=>{
+      const buf=await blob.arrayBuffer();
+      sendResponse({ok:true,buffer:Array.from(new Uint8Array(buf)),type:blob.type});
+    }).catch(()=>sendResponse({ok:false}));
     return true;
   }
 });
